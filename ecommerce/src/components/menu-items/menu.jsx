90bf76bcf73fe524237fa18910035ac193ae3a17
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-19 10:56:34 -0400

    Menu items

--- ecommerce/src/components/menu-items/menu.jsx
+++ ecommerce/src/components/menu-items/menu.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
 import './menu.scss'
 
-const Menu =(props) =>{
 
-    return(
-        
-        <div className={` ${props.size} menu-item` }>
+const Menu = (props) => (
 
+  <div className={`${props.size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${props.image})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{props.title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
 
-            <div className="background-image" 
-
-            style={{backgroundImage : `url(${props.image})`}}
-            
-            />
-
-            <div className="content">
-                
-                <h1 className="title"> {props.title.toUpperCase()} </h1>
-                <span className="subtitle">SHOP NOW</span>           
-        </div>  
-                
-        </div>
-        
-    )
-
-}
 
 
 export default Menu

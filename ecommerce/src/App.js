7b976f27ed735df7b92378151245854f9abf9927
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-22 06:02:10 -0400

    Updated

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,27 +2,22 @@ import React from 'react';
 import './App.css';
 import {Route ,Switch ,Link} from 'react-router-dom'
 import Homepage from './components/homepage/homepage'
+import Shop from './components/shop/shop'
+import Header from './components/Header/header_file'
 
-const HatsPage =(props) =>{
 
-  console.log(props)
-  
-  return(
-    <div>     
-      <h1>Hats page Created</h1>
-    </div>
-    
-  )
-}
 const App =() =>{
      
   return (
     <div >
+
+     <Header/>
      
      <Switch>
-
+  
         <Route exact={true}  path ='/'  component={Homepage}/>
-        <Route exact={true}  path ='/hats-page/'  component={HatsPage}/>
+        <Route exact={true}  path ='/shop/'  component={Shop}/>
+        
 
     </Switch> 
     

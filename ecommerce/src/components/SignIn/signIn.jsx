56017a61a9f8ccd83d2368485b568628c972434f
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-22 14:50:11 -0400

    SignIn Page

---
+++ ecommerce/src/components/SignIn/signIn.jsx
@@ -0,0 +1,68 @@
+import React,{useState} from 'react'
+import './signIn.scss'
+import FormInput from '../Form-Input/FormInput'
+import MyButtons from '../Buttons/buttons'
+
+
+//In order to style input elements of form we make another component 
+const SignIn =() =>{
+
+    const[ inputText ,setinputText] =useState({
+
+        name : '',
+        password : ''
+
+    });
+   
+    const submitting =(event) =>{
+
+        event.preventDefault();
+        alert("sucessfully")
+
+    }
+
+    const Changer =(name ,value) =>{
+
+       setinputText((prevValue) => {
+        return {
+            ...prevValue,
+            [name]: value
+        };
+    });
+
+
+    }
+
+        return(
+            <div className="sign-in">
+
+                <h2 className="title">I already have an account</h2>
+                <span>Sign IN with email and password</span>
+
+                <form onSubmit={submitting}>
+                   
+                    <FormInput 
+                    type="email"
+                    name="email" 
+                    label="Email" 
+                    value={inputText.email}  
+                    handleChange={Changer} required/>
+
+                   
+                    <FormInput 
+                    type="password"
+                    name="password" 
+                    label="Password" 
+                    handleChange={Changer} required/>
+
+
+                    <MyButtons> SIGN IN </MyButtons>
+
+                </form>
+
+            </div>
+        )
+    
+}
+
+export default SignIn

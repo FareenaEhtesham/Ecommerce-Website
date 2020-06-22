4b06512dd34e982298556267b1883667c41cc281
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-22 06:00:50 -0400

    Header Component with routing

---
+++ ecommerce/src/components/Header/header_file.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './header_file.scss'
+import {Link} from 'react-router-dom'
+import {ReactComponent as LogoWeb} from '../../assets/crown.svg'
+
+
+const Header =() =>{
+
+    return(
+        <div className="header"> 
+
+        <Link to='/' className="logo-container">
+
+            <LogoWeb className="logo"/>
+        </Link>
+
+        <div className="options">
+        <Link className="option" to='/shop' >
+
+        SHOP
+
+        </Link>
+
+        <Link className="option" to='/shop'>
+
+        CONTACT
+        
+        </Link>
+       
+        </div>  
+        </div>
+    )
+
+}
+
+export default Header

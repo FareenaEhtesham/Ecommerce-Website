b72279130a6cfdbc1f0418a20b3cc65c614a8e2f
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-22 14:52:24 -0400

    Updated

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -4,6 +4,7 @@ import {Route ,Switch ,Link} from 'react-router-dom'
 import Homepage from './components/homepage/homepage'
 import Shop from './components/shop/shop'
 import Header from './components/Header/header_file'
+import SignInUp from './components/page/SignIn-&-SignUp/signIn-signUp'
 
 
 const App =() =>{
@@ -17,6 +18,7 @@ const App =() =>{
   
         <Route exact={true}  path ='/'  component={Homepage}/>
         <Route exact={true}  path ='/shop/'  component={Shop}/>
+        <Route exact={true}  path ='/sign-in/'  component={SignInUp}/>
         
 
     </Switch> 
